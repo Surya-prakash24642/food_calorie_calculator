287f352b7c445e6d20dcafe7760414cfca28ac00
Surya-prakash24642 <surya.k@jmangroup.com>
2025-02-24 14:57:46 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FoodSearch from './FoodSearch'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center'>
+        <FoodSearch/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
